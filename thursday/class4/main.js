d14399d69171a29d8d36d3bbf9c82a9bcc721dda
hamzah-syed <hamzah.syed17@gmail.com>
2023-08-11 15:35:51 +0500

    code added

---
+++ thursday/class4/main.js
@@ -0,0 +1,41 @@
+"use strict";
+// function greet(firstName: string, age: number) {
+//   console.log(`Hello ${firstName} your age is ${age}`);
+// }
+// greet("Hamzah", 22);
+// greet("Arham", 24);
+// function calc(kg: number) {
+//   let result =  Math.round(kg / 7);
+//   console.log(`${result} KG`);
+//   return result;
+// }
+// let calculatedMeat = calc(100); // 14
+// console.log(calculatedMeat);
+// // if (calculatedMeat > 15) {
+// //   console.log("Pack in 2 Shoppers");
+// // } else {
+// //   console.log("Pack in one Shooper");
+// // }
+// function IsEvenOrOdd(Value: number) {
+//   if (Value % 2 == 0) {
+//     console.log("Even");
+//   } else {
+//     console.log("odd");
+//   }
+// }
+// IsEvenOrOdd(23);
+// let i = 0;
+// i++
+// i++
+// i++
+// console.log(i) 
+// for (let i = 0; i <= 100; i++ ) {
+//     console.log(i)
+// }
+//              0          1         2        3         4
+let fruits = ['Apple', 'Orange', 'Banana', "Mango", "Pineapple"];
+// 5
+for (let i = 0; i < fruits.length; i++) {
+    let fruit = fruits[i];
+    console.log(fruit);
+}
---
+++ wednesday/class4/main.ts
@@ -0,0 +1,70 @@
+// function greet(name: string, age: number) {
+//   let message = `Hello ${name}, you are ${age} years old!`;
+
+//   return message;
+// }
+
+// let message = greet("Hamzah", 22); // Hello Hamzah, you are 22 years
+
+// function EvenOdd(num: number) {
+//   if (num % 2 == 0) {
+//     return "Even Number";
+//   } else {
+//     return "Odd Number";
+//   }
+// }
+
+// let checknum = 8;
+// let result = EvenOdd(checknum);
+// console.log(result);
+
+// let i = 0;
+// i++ // 1
+// i++ // 2
+
+// console.log(i);
+
+// let i = 0;
+// for (i; i < 3; i++) {
+//   console.log("Hello");
+// }
+
+// for(let i = 100; i >= 0; i--){
+//     console.log(i)
+// }
+
+// var cleanestCities = ["Karachi", "Lahore", "Islamabad", "Peshawar"];
+
+// for (var i = 0; i <= 4; i++) {
+
+// }
+
+// let i = 0
+
+//              0          1         2        3         4          5
+// let fruits = ["Apple", "Orange", "Banana", "Mango", "Grapes", "Pineapple"];
+
+// for(let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i])
+// }
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// var fullNames = [];
+
+// for (var i = 0; i < firstNames.length; i++) {
+//   for (var j = 0; j < lastNames.length; j++) {
+//     fullNames.push(firstNames[i] + lastNames[j]);
+//   }
+// }
+
+for (let i = 0; i < 3; i++) {
+  console.log("Loop");
+
+  for (let j = 0; j < 3; j++) {
+    console.log("Nested Loop");
+  }
+
+
+  
+}
