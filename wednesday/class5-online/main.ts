77edfca9eda1669b8985d132451fbb61b1bcd26c
hamzah-syed <hamzah.syed17@gmail.com>
2023-08-17 09:44:27 +0500

    class 5-online code added

---
+++ wednesday/class5-online/main.ts
@@ -0,0 +1,33 @@
+// let username = "hamzah";
+// let age = 20;
+// let isMarried = true;
+
+// let nic = {
+//   dateOfBirth: "1999-01-01",
+//   fatherName: "Ali",
+//   name: "hamzah",
+//   address: "lahore",
+// };
+
+// let car1 = {
+//   name: "BMW", // properties
+//   color: "red", // properties
+//   model: 2020, // properties
+//   start: function () {
+//     // method
+//     console.log("Engine started");
+//   },
+// };
+
+// car1.start();
+
+// // Convert into TS Object
+
+// const a = 5;
+// const b: number = 33;
+
+// const c = "hamzah";
+
+import { num1, greet } from "./calculator";
+
+greet();
